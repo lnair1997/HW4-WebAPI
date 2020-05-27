25bb92442810028e076800fde5e625ee15465c66
lathisha <lathisha.n@gmail.com>
2020-05-27 00:26:56 -0700

    need local storage

--- assets/script.js
+++ assets/script.js
@@ -1,90 +1,156 @@
-var mainContainer= document.querySelectorAll(".container-md");
-var content= document.getElementById("content");
-var startBtn= document.getElementById("start");
+// Variables from HTML
+var quizContainer = document.querySelector("#quizContainer");
+var startBtn = document.querySelector("#startBtn");
+var questionsDis = document.querySelector("#questionsDis");
+var countDown = document.querySelector("#countDown");
+var currentQ = document.querySelector("#currentQ");
+var options = document.querySelector("#options");
+var results = document.querySelector("#results");
+var scoreScreen = document.querySelector("#scoreScreen");
+var finalScore = document.querySelector("#finalScore");
+var highScores = document.querySelector("#highScores");
+var initial = document.querySelector("#initial");
+var submitInitial = document.querySelector("#submitInitial");
+var highScoreList = document.querySelector("#highScoreList");
 
-var score;
-var time;
-var quizTime;
-var highScores=[];
 var quiz = [
     {
-        question: "Which of the following is correct about JavaScript?",
-        1: "JavaScript is a lightweight, interpreted programming language",
-        2: "JavaScript has object-oriented capabilities that allows you to build interactivity into otherwise static HTML pages",
-        3: "The general-purpose core of the language has been embedded in Netscape, Internet Explorer, and other web browsers",
-        4: "All of the above",
-        correct: 4
+        question: "Commonly used data types DO NOT include:",
+        answers: ["strings", "booleans", "alerts", "numbers"],
+        correct: "alerts"
     },
     {
-        question:
-            "Which of the following type of variable is visible everywhere in your JavaScript code?",
-        1: "global variable",
-        2: "local variable",
-        3: "Both of the above",
-        4: "None of the above",
-        correct: 1
+        question: "Inside which HTML element do we put the JavaScript?",
+        answers: ["<js></js>", "<javascript></javascript>",
+            "<script></script>", "<scripting></scripting>"],
+        correct: "<script></script>"
     },
     {
-        question:
-            "Which built-in method combines the text of two strings and returns a new string?",
-        1: "append",
-        2: "concat",
-        3: "attach",
-        4: "None of the above",
-        correct: 2
+        question: 'How do you write "Hello World" in an alert box?',
+        answers: ['alert("Hello World");', 'alertBox("Hello World");',
+            'msg("Hello World");', 'msgBox("Hello World");'],
+        correct: 'alert("Hello World");'
     },
     {
-        question:
-            "Which built-in method adds one or more elements to the end of an array and returns the new length of the array?",
-        1: "last",
-        2: "put",
-        3: "push",
-        4: "None of the above",
-        correct: 3
+        question: "The external JavaScript file must contain the <script> tag.",
+        answers: ["True", "False",],
+        correct: "True"
     },
     {
-        question:
-            "Which built-in method returns the calling string value converted to lower case?",
-        1: "toLowerCase",
-        2: "toLower",
-        3: "changeCase(case)",
-        4: "None of the above",
-        correct: 1
+        question: "What is the HTML tag under which one can write the JavaScript code?",
+        answers: ["javascript", "scripted", "script", "js"],
+        correct: "script"
     },
     {
-        question:
-            "Which of the following function of Array object extracts a section of an array and returns a new array?",
-        1: "reverse()",
-        2: "shift()",
-        3: "slice()",
-        4: "some()",
-        correct: 3
-    }
+        question: "How do you create a function in JavaScript?",
+        answers: ["function = myFunction()", "function:myFunction()",
+            "function myFunction()"],
+        correct: "function myFunction()"
+    },
+    {
+        question: "How do you write an IF statement in JavaScript?",
+        answers: ["if i = 5 then", "if i == 5 then", "if i = 5", "if (i == 5)"],
+        correct: "if (i == 5)"
+    },
+    {
+        question: "How does a FOR loop start?",
+        answers: ["for (i = 0; i <= 5; i++)", "for i = 1 to 5",
+            "for (i <= 5; i++)", "for (i = 0; i <= 5)"],
+        correct: "for (i = 0; i <= 5; i++)"
+    },
 ];
 
-startBtn.addEventListener("click", function() {
-    // score at zero
-    score= 0;
-    // timer to 60 sec
-    time= 60;
+var secondsRemaining = 120;
+var scores = 0;
+var timerInterval = "";
 
-    var timer= document.createElement("h2");
-    timer.setAttribute("id", "timer");
-    timer.textContent = "Time remaining: ";
-    document.getElementById("content").appendChild(timer);
-    
-    var span= document.createElement("span");
-    span.setAttribute("id", "countdown");
-    span.textContent = time;
-    document.getElementById("timer").appendChild(span);
+// 1)
+startBtn.addEventListener("click", function () {
+    startBtn.style.display = "none";
+    quizContainer.style.display = "none";
+    renderQuestion();
+    questionsDis.style.display = "block";
 
-    quizTime = setInterval(function() {
-        time--;
-        if (time <= 0) {
-            clearInterval(quizTime);
-            endQuiz();
+    timerInterval = setInterval(function () {
+        secondsRemaining--;
+
+        if (secondsRemaining <= 0) {
+            clearInterval(timerInterval);
+            alert("Oops! Time out");
+            timeOver();
         }
-    })
+        countDown.textContent = secondsRemaining + "sec";
+    }, 1000);
+})
 
-    
+var lastQuestion = quiz.length - 1;
+var currentQuestion = 0;
+var allowSelection = false;
+
+// 2)
+function renderQuestion() {
+    allowSelection = true;
+    // Clear the list element
+    options.innerHTML = "";
+    results.innerHTML = "";
+    currentQ.textContent = quiz[currentQuestion].question;
+
+    for (var i = 0; i < quiz[currentQuestion].answers.length; i++) {
+        var answer = quiz[currentQuestion].answers[i]
+
+        var li = document.createElement("li");
+        var button = document.createElement("button");
+        button.classList.add("btn");
+        button.style.backgroundColor = "#FFFFD8";
+        button.textContent = answer;
+        li.appendChild(button);
+        options.appendChild(li);
+    }
+};
+
+// 3)
+options.addEventListener("click", function (event) {
+    if (allowSelection === true) {
+        var userChoice = event.target.textContent;
+
+        if (userChoice === quiz[currentQuestion].correct) {
+            scores++;
+            results.textContent = "Correct! Good Job!";
+            results.style.color = "#3eda8c";
+        } else {
+            results.textContent = "Wrong!!";
+            secondsRemaining = secondsRemaining - 5;
+            countDown.textContent = secondsRemaining + "s";
+            results.style.color = "#da3e3e";
+        } if (currentQuestion < lastQuestion) {
+            currentQuestion++;
+            setTimeout(renderQuestion, 1000);
+        } else {
+            clearInterval(timerInterval);
+            timeOver();
+        }
+    }
+    allowSelection = false;
 });
+
+// 4)
+function timeOver() {
+    questionsDis.style.display = "none";
+    scoreScreen.style.display = "block";
+    finalScore.textContent = scores;
+    finalScore.style.color = "#ec1515";
+};
+
+
+
+var highscores = [];
+var storedHighscores = JSON.parse(localStorage.getItem("highscores"));
+
+// 5)
+submitInitial.addEventListener("click", function () {
+    highScores.style.display = "block";
+    scoreScreen.style.display = "none";
+    localStorage.setItem("scores", finalScore);
+    localStorage.setItem("initials", initial.value);
+});
+
